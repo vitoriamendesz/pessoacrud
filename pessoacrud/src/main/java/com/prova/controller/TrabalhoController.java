42318f01a89b0b071c6c42881e66d281f1bb6f2b
vitoriamendesz <143222984+vitoriamendesz@users.noreply.github.com>
2025-04-15 20:19:11 -0300

    Update TrabalhoController.java

--- pessoacrud/src/main/java/com/prova/controller/TrabalhoController.java
+++ pessoacrud/src/main/java/com/prova/controller/TrabalhoController.java
@@ -1,7 +1,7 @@
-package com.prova.controller;
+package com.pessoa.pessoa.controller;
 
-import com.prova.entity.Trabalho;
-import com.prova.repository.TrabalhoRepository;
+import com.pessoa.pessoa.entity.Trabalho;
+import com.pessoa.pessoa.repository.TrabalhoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
